30ab1d1f59156178b408bd5d7106f743fb7adaee
taxyf <taxyf@163.com>
2024-09-17 21:40:50 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["200.228.1.20"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["200.228.1.19"];
 pub const PUBLIC_RS_PUB_KEY: &str = "61156238";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
